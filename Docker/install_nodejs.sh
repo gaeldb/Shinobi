72139644d895c684bfad5ac0dd73889576bd5557
Moe <github@m03.ca>
2023-10-26 10:38:01 -0700

    fix some scripts for Docker install

--- Docker/install_nodejs.sh
+++ Docker/install_nodejs.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
-which node > /dev/null
-
-if [ "$?" -ne 0 ] ; then 
+if [ -x "$(command -v node)" ]; then
+    echo "Node.js detected. Version : $(node -v)"
+else
     echo "Installing NodeJS 18"
     mkdir -p /etc/apt/keyrings
     apt-get install -y ca-certificates gnupg
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-    
+
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-    
-    apt-get update -y --fix-missing 
-    apt-get upgrade -y 
+
+    apt-get update -y --fix-missing
+    apt-get upgrade -y
     apt-get install nodejs -y
-fi 
+fi
 
-node -v
-npm -v
-
-npm i npm@latest pm2 pg -g --save
-npm install --unsafe-perm
+if [ -x "$(command -v npm)" ]; then
+    echo "NPM detected. Version : $(npm -v)"
+    npm i npm@latest pm2 pg -g --save
+    npm install --unsafe-perm
+else
+    echo "NPM NOT FOUND!"
+fi
