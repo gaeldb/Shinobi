3b875473a14f1924bc1a7a4b5469c8e6472051fe
Moe <github@m03.ca>
2024-09-08 10:49:05 -0700

    update nodejs installer for ubuntu 18.04 (jetson nano)

--- INSTALL/nodejs-ubuntu.sh
+++ INSTALL/nodejs-ubuntu.sh
@@ -1,8 +1,26 @@
+#!/bin/bash
+
+# Get the Ubuntu version
+UBUNTU_VERSION=$(lsb_release -rs)
+
+# Determine the Node.js major version based on Ubuntu version
+if [[ $(echo "$UBUNTU_VERSION <= 18.04" | bc -l) -eq 1 ]]; then
+    NODE_MAJOR=16
+else
+    NODE_MAJOR=18
+fi
+
+# Update and install necessary packages
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
+
+# Setup NodeSource keyring and sources list
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=18
+
+# Add NodeSource repository
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+# Update package list and install Node.js
 sudo apt-get update
 sudo apt-get install nodejs -y
